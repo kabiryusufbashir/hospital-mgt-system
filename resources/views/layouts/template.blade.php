457cd6562263a8d964b0e97981b3ddca74cf9910
Piccolo <kabiryusufbashir@gmail.com>
2021-02-24 17:15:50 +0100

    Create Login form

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,9 +4,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="Icon" href="{{ asset('images/favicon.ico') }}" type="image/icon">
     <title>@yield('title')</title>
 </head>
-<body>
-    
+<body class="bg-gray-100">
+    @yield('content')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,32 @@
 @extends('layouts.template')
 
-@section('Hospital Mgt System')
+@section('title')
+Hospital Mgt System
+@endsection
+
+@section('content')
+    <div class="sm:w-1/3 sm:mx-auto my-24 p-5 bg-white rounded shadow-md">
+        <div>
+            <h2 class="text-center text-3xl text-blue-600 mb-4">Piccolo Hospital Mgt. System</h2>
+            <img class="w-28 mx-auto" src="{{ asset('images/logo.png') }}" alt="Logo">
+        </div>
+        <div>
+            <form action="#">
+                <div>
+                    <label class="font-medium" for="username">Username</label><br>
+                    <input type="text" name="username" placeholder="Username" class="px-5 w-full border border-gray-300 h-12 rounded-full my-2 text-lg">
+                </div>
+                <div>
+                    <label class="font-medium" for="password">Password</label><br>
+                    <input type="password" name="password" placeholder="Password" class="px-5 w-full border border-gray-300 h-12 rounded-full my-2 text-lg">
+                </div>
+                <div class="text-right mb-4">
+                    <span><a href="#">Forgot your Password?</a></span>
+                </div>
+                <div class="text-center">
+                    <button class="bg-blue-600 py-2 text-white rounded-full uppercase w-full h-12">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
