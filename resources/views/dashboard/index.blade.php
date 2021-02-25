74f3c4e7029d543daa3ba41cb702807d937a591e
Piccolo <kabiryusufbashir@gmail.com>
2021-02-25 17:31:34 +0100

    Start desigining the dashboard layout II

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,5 +5,15 @@
 @endsection
 
 @section('content')
-    
+    <div class="bg-blue-600 h-16">
+        header section
+    </div>
+    <div class="grid grid-cols-4">
+        <div class="bg-white col-span-1">
+            Nav
+        </div>
+        <div class="bg-green-300 col-span-3">
+            Info
+        </div>
+    </div>
 @endsection 
