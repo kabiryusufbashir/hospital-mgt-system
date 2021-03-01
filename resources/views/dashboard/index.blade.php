dcc16046440eec1c294c770dedc4c6c630e0be9b
Piccolo <kabiryusufbashir@gmail.com>
2021-03-01 17:31:15 +0100

    The page is responsive on small devices and i will call it a day for now!

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,16 +6,16 @@
 
 @section('content')
     <!-- Nav  -->
-    <div class="bg-blue-600 h-16">
-        <div class="flex justify-between grid grid-cols-4 py-3">
+    <div class="bg-blue-600">
+        <div class="md:flex md:justify-between md:grid md:grid-cols-4 py-3">
             <!-- system details -->
-            <div class="ml-3 flex items-center justify-between col-span-1">
+            <div class="mx-3 flex items-center justify-between col-span-1">
                 <img class="w-10" src="{{ $system_settings->photo ?? asset('images/logo.png') }}" alt="Logo">
-                <h2 class="text-white ml-2">{{$system_settings->hospital_name ?? 'Piccolo Hospital Mgt. System'}}</h2>
+                <h2 class="text-white ml-2 text-lg">{{$system_settings->hospital_name ?? 'Piccolo Hospital Mgt. System'}}</h2>
                 <svg id="menuCaret" class="w-8 h-8 text-white cursor-pointer" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </div>
             <!-- user details -->
-            <div class="flex items-center justify-end col-span-3 mr-3">
+            <div class="flex justify-center mt-4 md:mt-0 items-center md:justify-end col-span-3 mr-3">
                 <!-- Notifications -->
                 <div class="flex relative">
                     <svg class="w-6 h-6 text-white mx-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path></svg></span>
@@ -30,7 +30,7 @@
                 <div class="flex text-white items-center relative inline-block">
                     <span class="flex cursor-pointer" id="user_caret">
                         <svg class="w-6 h-6 text-white ml-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-                        {{auth()->user()->username}}
+                        <b>{{auth()->user()->username}}</b>
                         <svg id="caret" class="w-5 h-5 my-auto" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>    
                     </span>
                     <div id="user_caret_menu" class="hidden absolute top-5 right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -61,9 +61,9 @@
     </div>
 
     <!-- Menu  -->
-    <div class="grid grid-cols-5">
+    <div class="md:grid md:grid-cols-5">
         <!-- Nav link  -->
-        <div id="menu" class="px-3 bg-white col-span-1 transition duration-500 ease-in-out">
+        <div id="menu" class="px-3 bg-white col-span-1">
             <h2 class="text-2xl py-3 border-b font-medium">Menu</h2>
             <ul>
                 <li class="py-3 flex border-b">
