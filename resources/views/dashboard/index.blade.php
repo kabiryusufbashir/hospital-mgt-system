c921318446a7216160ee4969bd65dba4c8870070
Piccolo <kabiryusufbashir@gmail.com>
2021-03-01 14:55:51 +0100

    Working on the main menu

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -59,8 +59,9 @@
                     <a href="#">Dashboard</a>
                 </li>
                 <li class="py-3 flex border-b">
-                   <img class="w-7 mr-4" src="{{ asset('images/doctor_icon.png') }}" alt="Doctor">
+                    <img class="w-7 mr-4" src="{{ asset('images/doctor_icon.png') }}" alt="Doctor">
                     <a href="#">Doctors</a>
+                    <svg class="w-5 h-5 ml-auto my-auto" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                 </li>
                 <li class="py-3 flex border-b">
                     <img class="w-7 mr-4" src="{{ asset('images/nurse.png') }}" alt="Nurse">
