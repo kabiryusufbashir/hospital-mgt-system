9287876df927c59ac9012e40b76f04971df72e41
Piccolo <kabiryusufbashir@gmail.com>
2021-03-01 15:51:07 +0100

    Done with Lab, Accounts & Patients

--- public/js/main.js
+++ public/js/main.js
@@ -66,3 +66,54 @@ pharmacistCaret.addEventListener('click', ()=>{
         pharmacistCaret.classList.remove('roate-90');
     }
 });
+
+// laboratorist caret 
+const laboratoristCaret = document.querySelector("#laboratoristCaret");
+const laboratoristMenu = document.querySelector("#laboratoristMenu");
+
+laboratoristCaret.addEventListener('click', ()=>{
+    if(laboratoristMenu.classList.contains('hidden')){
+        laboratoristMenu.classList.remove('hidden');
+        laboratoristCaret.classList.add('transform');
+        laboratoristCaret.classList.add('rotate-90');
+        laboratoristCaret.classList.add('transition');
+    }else{
+        laboratoristMenu.classList.add('hidden');
+        laboratoristCaret.classList.remove('transform');
+        laboratoristCaret.classList.remove('roate-90');
+    }
+});
+
+// accountant caret 
+const accountantCaret = document.querySelector("#accountantCaret");
+const accountantMenu = document.querySelector("#accountantMenu");
+
+accountantCaret.addEventListener('click', ()=>{
+    if(accountantMenu.classList.contains('hidden')){
+        accountantMenu.classList.remove('hidden');
+        accountantCaret.classList.add('transform');
+        accountantCaret.classList.add('rotate-90');
+        accountantCaret.classList.add('transition');
+    }else{
+        accountantMenu.classList.add('hidden');
+        accountantCaret.classList.remove('transform');
+        accountantCaret.classList.remove('roate-90');
+    }
+});
+
+// Patient caret 
+const patientCaret = document.querySelector("#patientCaret");
+const patientMenu = document.querySelector("#patientMenu");
+
+patientCaret.addEventListener('click', ()=>{
+    if(patientMenu.classList.contains('hidden')){
+        patientMenu.classList.remove('hidden');
+        patientCaret.classList.add('transform');
+        patientCaret.classList.add('rotate-90');
+        patientCaret.classList.add('transition');
+    }else{
+        patientMenu.classList.add('hidden');
+        patientCaret.classList.remove('transform');
+        patientCaret.classList.remove('roate-90');
+    }
+});
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+    <!-- Nav  -->
     <div class="bg-blue-600 h-16">
         <div class="flex justify-between grid grid-cols-4 py-3">
             <div class="ml-3 flex items-center justify-between col-span-1">
@@ -50,6 +51,8 @@
             </div>
         </div>
     </div>
+
+    <!-- Menu  -->
     <div class="grid grid-cols-5">
         <div class="px-3 bg-white col-span-1">
             <h2 class="text-2xl py-3 border-b font-medium">Menu</h2>
@@ -58,6 +61,7 @@
                     <img class="w-7 mr-4" src="{{ asset('images/dashboard.png') }}" alt="Dashboard">
                     <a href="#">Dashboard</a>
                 </li>
+                
                 <!-- Doctor -->
                 <li class="py-3 flex border-b">
                     <img class="w-7 mr-4" src="{{ asset('images/doctor_icon.png') }}" alt="Doctor">
@@ -72,6 +76,7 @@
                         <a href="#">All Doctors</a>
                     </li>
                 </div>
+                
                 <!-- Nurse  -->
                 <li class="py-3 flex border-t border-b">
                     <img class="w-7 mr-4" src="{{ asset('images/nurse.png') }}" alt="Nurse">
@@ -86,6 +91,7 @@
                         <a href="#">All Nurse</a>
                     </li>
                 </div>
+                
                 <!-- Pharmacist  -->
                 <li class="py-3 flex border-t border-b"> 
                     <img class="w-7 mr-4" src="{{ asset('images/pharmacist.png') }}" alt="Pharmacists">
@@ -101,18 +107,52 @@
                     </li>
                 </div>
 
+                <!-- Laboratorist -->
                 <li class="py-3 flex border-t">
                     <img class="w-7 mr-4" src="{{ asset('images/laboratorist.png') }}" alt="Laboratorist">
                     <a href="#">Laboratorists</a>
+                    <svg id="laboratoristCaret" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                 </li>
+                <div id="laboratoristMenu" class="users-caret-menu hidden">
+                    <li class="py-3 flex border-b">
+                        <a href="#">Add Laboratorist</a>
+                    </li>
+                    <li class="py-3 flex">
+                        <a href="#">All Laboratorists</a>
+                    </li>
+                </div>
+                
+                <!-- Accountants  -->
                 <li class="py-3 flex border-t">
                     <img class="w-7 mr-4" src="{{ asset('images/accounting.png') }}" alt="Accounting">
                     <a href="#">Accountants</a>
+                    <svg id="accountantCaret" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                 </li>
+                <div id="accountantMenu" class="users-caret-menu hidden">
+                    <li class="py-3 flex border-b">
+                        <a href="#">Add Accountant</a>
+                    </li>
+                    <li class="py-3 flex">
+                        <a href="#">All Accountants</a>
+                    </li>
+                </div>
+
+                <!-- patient  -->
                 <li class="py-3 flex border-t">
                     <img class="w-7 mr-4" src="{{ asset('images/patient.png') }}" alt="Patient">
                     <a href="#">Patients</a>
+                    <svg id="patientCaret" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                 </li>
+                <div id="patientMenu" class="users-caret-menu hidden">
+                    <li class="py-3 flex border-b">
+                        <a href="#">Add Patient</a>
+                    </li>
+                    <li class="py-3 flex">
+                        <a href="#">All Patients</a>
+                    </li>
+                </div>
+
+                <!-- Manage Hospital -->
                 <li class="py-3 flex border-t">
                     <img class="w-7 mr-4" src="{{ asset('images/manage.png') }}" alt="Manage">
                     <a href="#">Manage Hospital</a>
