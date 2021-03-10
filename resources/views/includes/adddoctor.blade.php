c9487e04d7ff74764fe904ebd6b7bc26ef79d04a
Piccolo <kabiryusufbashir@gmail.com>
2021-03-10 12:13:10 +0100

    Design Modal sample

--- resources/views/includes/adddoctor.blade.php
+++ resources/views/includes/adddoctor.blade.php
@@ -5,39 +5,41 @@
         
         <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
         
-        <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
-            <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-            </svg>
-            <span class="text-sm">(Esc)</span>
-        </div>
-
-        <!-- Add margin if you want to see some of the overlay behind the modal-->
-        <div class="modal-content py-4 text-left px-6">
-            <!--Title-->
-            <div class="flex justify-between items-center pb-3">
-            <p class="text-2xl font-bold">Simple Modal!</p>
-            <div class="modal-close cursor-pointer z-50">
-                <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+            <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
                 <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
                 </svg>
-            </div>
+                <span class="text-sm">(Esc)</span>
             </div>
 
-            <!--Body-->
-            <p>Modal content can go here</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
+            <!-- Add margin if you want to see some of the overlay behind the modal-->
+            <div class="modal-content text-left">
+                <!--Title-->
+                <div class="bg-blue-600 flex justify-between items-center py-4 text-white">
+                    <p class="text-2xl font-bold px-6">Add Doctor</p>
+                    <div class="modal-close cursor-pointer z-50 px-6">
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                        </svg>
+                    </div>
+                </div>
 
-            <!--Footer-->
-            <div class="flex justify-end pt-2">
-            <button class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
-            <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+                <!--Body-->
+                <div class="px-6 py-4">
+                    <p>Modal content can go here</p>
+                    <p>...</p>
+                    <p>...</p>
+                    <p>...</p>
+                    <p>...</p>
+                </div>
+
+                <!--Footer-->
+                <div class="px-6 py-4 flex justify-end border-t">
+                    <button class="modal-close px-4 bg-blue-600 rounded-lg text-white hover:bg-blue-800 py-2">Add</button>
+                </div>
+                
             </div>
             
-        </div>
         </div>
     </div>
 
